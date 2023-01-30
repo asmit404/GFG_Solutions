d7fcfec7042a952ba5c9403860df53e585dc6584
Asmit Singh <asmittbhardwaj@gmail.com>
2023-01-30 19:04:40 +0530

    Create Select Nodes.py

---
+++ Select Nodes.py
@@ -0,0 +1,25 @@
+'''
+Title     : Select Nodes
+Domain    : Dynamic Programming, DFS, Tree, Data Structures, Algorithms
+Author    : Asmit Singh
+Solved On   : 30 Jan 2023
+Solved Using   : Python3
+'''
+
+class Solution:
+    def dfs(self, graph, u, p):
+        exc, inc = 0, 1    
+        for v in graph[u]:
+            if v != p:
+                cexc, cinc = self.dfs(graph, v, u)
+                exc += cinc
+                inc += min(cinc, cexc)
+        return exc, inc
+
+    def countVertex(self, N, edges):
+        graph = [[] for _ in range(N)]
+        for u,v in edges:
+            graph[u-1].append(v-1)
+            graph[v-1].append(u-1)
+            
+        return min(self.dfs(graph, 0, -1))
