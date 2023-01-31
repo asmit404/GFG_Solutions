ff44b7c52eca304ca376b00dbc91ceb0604a1b73
Asmit Singh <asmittbhardwaj@gmail.com>
2023-01-31 19:57:02 +0530

    Create Min Repeat Substring.py

---
+++ Min Repeat Substring.py
@@ -0,0 +1,28 @@
+'''
+Title     : Minimum times A has to be repeated such that B is a substring of it
+Domain    : Searching, Strings, Pateern Searching, Data Structures, Algorithms
+Author    : Asmit Singh
+Solved On   : 31 Jan 2023
+Solved Using   : Python3
+'''
+
+class Solution: 
+    def issubstring(self, str2, rep):
+        if str2 in rep:
+            return True
+        return False
+    
+    def minRepeats(self, A, B):
+        ans = 1
+        S = A
+        
+        while len(S)<len(B):
+            S += A 
+            ans += 1 
+            
+        if self.issubstring(B,S):
+            return ans 
+        elif self.issubstring(B,S+A):
+            return ans+1 
+        else:
+            return -1
