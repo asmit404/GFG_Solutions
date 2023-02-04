8171b7e35168780e04a77ef1f9e259307bd5a820
Asmit Singh <asmittbhardwaj@gmail.com>
2023-02-04 22:26:27 +0530

    Update Max Sum without Adjacents.py

--- Max Sum without Adjacents.py
+++ Max Sum without Adjacents.py
@@ -10,7 +10,6 @@ class Solution:
     def findMaxSum(self,arr, n):
         if n==1: return arr[0]
         if n==2: return max(arr[0], arr[1])
-
         dp = [-1 for i in range(n)]
         dp[0], dp[1] = arr[0], max(arr[0], arr[1])
         for i in range(2, n): dp[i] = max(dp[i-1], arr[i]+dp[i-2])
