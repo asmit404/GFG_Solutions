a6aca4128fcf7790df2f75dfaaa627ac33456b14
Asmit Singh <asmittbhardwaj@gmail.com>
2023-02-10 20:17:40 +0530

    Create Balloon Everywhere.py

---
+++ Balloon Everywhere.py
@@ -0,0 +1,21 @@
+'''
+Title     : Balloon Everywhere
+Domain    : Map, Data Structures
+Author    : Asmit Singh
+Solved On   : 10 Feb 2023
+Solved Using   : Python3
+'''
+
+from collections import Counter
+class Solution:
+    def maxInstance(self, s : str) -> int:
+        # code here
+        a=Counter('balloon')
+        b=Counter(s)
+        mini=1e9
+        for i in a:
+            if i in b:
+                mini=min(mini,b[i]//a[i])
+            else:
+                return 0
+        return mini
