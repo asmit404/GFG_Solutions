5380ca82a8b8ca7417057263adee285e5c4701c6
Asmit Singh <asmittbhardwaj@gmail.com>
2023-02-14 20:32:41 +0530

    Create Walls Coloring.py

---
+++ Walls Coloring.py
@@ -0,0 +1,16 @@
+'''
+Title     : Walls Coloring
+Domain    : Dynamic Programming
+Author    : Asmit Singh
+Solved On   : 14 Feb 2023
+Solved Using   : Python3
+
+'''
+
+class Solution():
+    def minCost(self, color, N):
+        for i in range(1,N):
+            color[i][0]+=min(color[i-1][1],color[i-1][2])
+            color[i][1]+=min(color[i-1][2],color[i-1][0])
+            color[i][2]+=min(color[i-1][1],color[i-1][0])
+        return min(color[-1]) 
