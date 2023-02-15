2708f5d8ee2cf19928bf3d2f2c1c0e81cdcaefcf
Asmit Singh <asmittbhardwaj@gmail.com>
2023-02-15 21:36:54 +0530

    Update Geeks Island.py

--- Geeks Island.py
+++ Geeks Island.py
@@ -18,8 +18,7 @@ class Solution():
                     Q.append((nx, ny))
                     visited.add((nx,ny))
         return visited
-            
-        
+
     def water_flow(self, mat, n, m):
         visited1 = self.bfs(mat, n, m, [(i,0) for i in range(n)] + [(0,j) for j in range(m)])
         visited2 = self.bfs(mat, n, m, [(i,m-1) for i in range(n)] + [(n-1,j) for j in range(m)])
