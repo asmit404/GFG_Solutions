4ee3caf226323f05126065fab670c963c083c78e
Asmit Singh <asmittbhardwaj@gmail.com>
2023-02-18 20:04:52 +0530

    Create Apple Sequences.py

---
+++ Apple Sequences.py
@@ -0,0 +1,27 @@
+'''
+Title     : Apple Sequences
+Domain    : Two Poimter Algorithm, Implementation, Algorithms
+Author    : Asmit Singh
+Solved On   : 18 Feb 2023
+Solved Using   : Python3
+'''
+
+class Solution:
+    def appleSequences(self, n, m, arr):
+        i , j = 0 , 0
+        counta = 0
+        counto = 0
+        ans = 0
+        while(j<n):
+            if(arr[j] == "A"):
+                counta += 1
+            elif(counto < m):
+                counto += 1
+            else:
+                while( i<j and arr[i] != "O"):
+                    i += 1
+                    counta -= 1
+                i += 1
+            ans = max(ans , counta + counto)
+            j += 1
+        return ans
