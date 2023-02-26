87f0dcf8bbf4154e5b0eeb30ce75c667b0e4a7bd
Asmit Singh <asmittbhardwaj@gmail.com>
2023-02-26 10:24:09 +0530

    Create Valid Compressed String.py

---
+++ Valid Compressed String.py
@@ -0,0 +1,30 @@
+'''
+Title     : Valid Compressed String
+Domain    : Strings, Greedy, Data Structures, Algorithms
+Author    : Asmit Singh
+Solved On   : 25 Feb 2023
+Solved Using   : Python3
+'''
+
+class Solution:
+    def checkCompressed(self, s, t):
+        i, j = 0, 0
+        slen = len(s)
+        tlen = len(t)
+        while i < slen and j < tlen:
+            if s[i] == t[j]:
+                i += 1
+                j += 1
+            elif t[j].isdigit():
+                num = 0
+                while j < tlen and t[j].isdigit():
+                    c = int(t[j])
+                    num = num*10+c
+                    j += 1
+                i = i+num
+            else:
+                return 0
+        if i == slen and j == tlen:
+            return 1
+        else:
+            return 0
