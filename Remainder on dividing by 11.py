e59c65aaabe93c471c0aaff53061ab61e4644c73
Asmit Singh <asmittbhardwaj@gmail.com>
2023-04-03 21:16:46 +0530

    Create Remainder on dividing by 11.py

---
+++ Remainder on dividing by 11.py
@@ -0,0 +1,11 @@
+"""
+Title     : Remainder on dividing by 11
+Domain    : Mathematical, Modular Arithmetic, Algorithms
+Author    : Asmit Singh
+Solved On   : 03 Apr 2023
+Solved Using   : Python3
+"""
+
+class Solution:
+    def xmod11(self, x):
+        return int(x) % 11
