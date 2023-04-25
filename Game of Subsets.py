ef3c2950259b6cd11187dd3299447d7e7015803f
Asmit Singh <asmittbhardwaj@gmail.com>
2023-04-25 18:05:01 +0530

    Create Game of Subsets.py

---
+++ Game of Subsets.py
@@ -0,0 +1,48 @@
+'''
+Title     : Game of Subsets
+Author    : Asmit Singh
+Solved On   : 24 Apr 2023
+Solved Using   : Python3
+'''
+
+from typing import List
+
+mod = 10**9+7
+mapper = [0]*31
+prime = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+
+for i in range(2, 31):
+    if (0 == i % 4 or 0 == i % 9 or 25 == i):
+        continue
+    mask = 0
+    for j in range(10):
+        if (0 == i % prime[j]):
+            mask |= 1 << j
+        mapper[i] = mask
+
+class Solution():
+    def goodSubsets(self, n, arr):
+        one = 0
+        dp = [0]*1024
+        cnt = [0]*31
+        dp[0] = 1
+        for i in arr:
+            if (i == 1):
+                one += 1
+            elif (mapper[i] != 0):
+                cnt[i] += 1
+        for i in range(31):
+            if (cnt[i] == 0):
+                continue
+            for j in range(1024):
+                if (0 != (j & mapper[i])):
+                    continue
+                dp[j | mapper[i]] = (
+                    (dp[j | mapper[i]] + dp[j] * cnt[i]) % mod)
+        ans = 0
+        for i in dp:
+            ans = (ans + i) % mod
+        ans -= 1
+        if (one != 0):
+            ans = ans * pow(2, one, mod) % mod
+        return ans
