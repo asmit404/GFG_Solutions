ecdb0d0124f6227694257ff871b7f298d6676e4a
Asmit Singh <asmittbhardwaj@gmail.com>
2023-05-08 17:38:04 +0530

    Create Binary Modulo.py

---
+++ Binary Modulo.py
@@ -0,0 +1,12 @@
+'''
+Title     : Binary Modulo
+Author    : Asmit Singh
+Solved On   : 8 May 2023
+Solved Using   : Python3
+'''
+
+class Solution():
+    def modulo(self, s, m):
+        k = int(s, 2)
+        r = k % m
+        return r
