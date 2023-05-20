a785c45c040217d35f71b5a33e3cfe07e94f873a
Asmit Singh <asmittbhardwaj@gmail.com>
2023-05-20 20:11:48 +0530

    Create Hands of Straights.py

---
+++ Hands of Straights.py
@@ -0,0 +1,22 @@
+'''
+Title     : Hands of Straights
+Author    : Asmit Singh
+Solved On   : 20 May 2023
+Solved Using   : Python3
+'''
+
+class Solution:
+    def isStraightHand(self, N, groupSize, hand):
+        mp = {}
+        for i in hand:
+            mp[i] = mp.get(i, 0)+1
+        hand.sort()
+        for card in hand:
+            if not mp[card]:
+                continue
+            for j in range(groupSize):
+                curCard = card + j
+                if mp.get(curCard, 0) == 0:
+                    return False
+                mp[curCard] -= 1
+        return True
