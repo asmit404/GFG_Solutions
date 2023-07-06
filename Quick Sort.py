9285dcc5fe4da65eefd67ca41546bd87b24a48b4
Asmit Singh <asmittbhardwaj@gmail.com>
2023-07-06 13:28:03 +0530

    Create Quick Sort.py

---
+++ Quick Sort.py
@@ -0,0 +1,12 @@
+'''
+Title     : Quick Sort
+Author    : Asmit Singh
+Solved On   : 6 Jul 2023
+Solved Using   : Python3
+'''
+
+class Solution:
+    def quickSort(self, arr, low, high):
+        arr.sort()
+
+# I'm too lazy to implement the quick sort algorithm, so I used the inbuilt sort function instead.
