68b80fcbda8c398c8765c89f4ed55c3a3c7a32aa
Asmit Singh <asmittbhardwaj@gmail.com>
2023-07-10 20:11:46 +0530

    Create Transpose of Matrix.py

---
+++ Transpose of Matrix.py
@@ -0,0 +1,12 @@
+'''
+Title     : Smallest Positive missing number
+Author    : Asmit Singh
+Solved On   : 9 Jul 2023
+Solved Using   : Python3
+'''
+
+class Solution:
+    def transpose(self, matrix, n):
+        for i in range(n):
+            for j in range(i+1):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
