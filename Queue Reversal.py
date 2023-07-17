87cb8e47cd18a47236eea68de72f814f99d98e5a
Asmit Singh <asmittbhardwaj@gmail.com>
2023-07-17 17:11:20 +0530

    Update Queue Reversal.py

--- Queue Reversal.py
+++ Queue Reversal.py
@@ -1,7 +1,7 @@
 '''
 Title     : Queue Reversal
 Author    : Asmit Singh
-Solved On   : 15 Jul 2023
+Solved On   : 16 Jul 2023
 Solved Using   : Python3
 '''
 
@@ -14,5 +14,5 @@ class Solution:
             q.put(stack.pop())
         return q
 
-# Time Complexity: IDFK, It took 5.74 seconds to execute.
+# IDFK why, but this took 5.74 seconds to execute.
 # If you have something better, send a PR.
