2ea4fb681841296e7398e94f426ec2aa2bf8ccbb
asmit404 <asmittbhardwaj@gmail.com>
2023-07-21 20:52:18 +0530

    Create Reverse a Linked List in groups of given size.py

---
+++ Reverse a Linked List in groups of given size.py
@@ -0,0 +1,26 @@
+"""
+Title     : Reverse a Linked List in groups of given size
+Author    : Asmit Singh
+Solved On   : 21 Jul 2023
+Solved Using   : Python3
+"""
+
+
+class Solution:
+    def reverse(self, head, k):
+        if head == None:
+            return None
+
+        prev, next = None, None
+        curr, count = head, 0
+
+        while curr != None and count < k:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+            count += 1
+
+        if next != None:
+            head.next = self.reverse(next, k)
+        return prev
