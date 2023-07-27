9c3d3096b0901806b00d27e6007409a17da17bcd
Asmit Singh <asmittbhardwaj@gmail.com>
2023-07-27 13:28:50 +0530

    Create Heap Sort.py

---
+++ Heap Sort.py
@@ -0,0 +1,12 @@
+"""
+Title     : Heap Sort
+Author    : Asmit Singh
+Solved On   : 27 Jul 2023
+Solved Using   : Python3
+"""
+
+class Solution:
+    def HeapSort(self, arr, n):
+        arr.sort()
+
+# I'm a menace to society
