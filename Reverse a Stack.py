a6d7b1974fcc9e60b18a38a3755af8926fabd9f9
Asmit Singh <asmittbhardwaj@gmail.com>
2023-08-04 21:05:33 +0530

    Create Reverse a Stack.py

---
+++ Reverse a Stack.py
@@ -0,0 +1,10 @@
+"""
+Title     : Reverse a Stack
+Author    : Asmit Singh
+Solved On   : 04 Aug 2023
+Solved Using   : Python3
+"""
+
+class Solution:
+    def reverse(self, St):
+        return St.reverse()
