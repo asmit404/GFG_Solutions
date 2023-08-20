fdd9ca628a5fcfe52d1b96533f8f9381f34bbfd0
Asmit Singh <asmittbhardwaj@gmail.com>
2023-08-20 13:18:07 +0530

    Create Number of occurrence.py

---
+++ Number of occurrence.py
@@ -0,0 +1,13 @@
+"""
+Title     : Number of occurence
+Author    : Asmit Singh
+Solved On   : 20 Aug 2023
+Solved Using   : Python3
+"""
+
+class Solution:
+    def count(self, arr, n, x):
+        return arr.count(x)
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
