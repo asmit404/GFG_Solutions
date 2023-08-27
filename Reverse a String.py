87f58724f57ddd70cdaf76ccdc197acd2c20791c
Asmit Singh <asmittbhardwaj@gmail.com>
2023-08-27 18:47:12 +0530

    Create Reverse a String.py

---
+++ Reverse a String.py
@@ -0,0 +1,13 @@
+"""
+Title     : Reverse a String
+Author    : Asmit Singh
+Solved On   : 27 Aug 2023
+Solved Using   : Python3
+"""
+
+class Solution:
+    def reverseWord(self, s):
+        return s[::-1]
+
+# Time Complexity : O(n)
+# Space Complexity : O(n)
