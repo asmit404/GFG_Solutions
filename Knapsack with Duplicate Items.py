f3531de5b6fe32d59e6f23f4d06a2fd7d31a47c4
asmit404 <asmittbhardwaj@gmail.com>
2023-10-25 20:08:15 +0530

    Create Knapsack with Duplicate Items.py

---
+++ Knapsack with Duplicate Items.py
@@ -0,0 +1,18 @@
+"""
+Title     : KnapSack with Duplicate Items
+Author    : Asmit Singh
+Solved On   : 25 Oct 2023
+Solved Using   : Python3
+"""
+
+class Solution:
+    def knapSack(self, N, W, val, wt):
+        dp = [0] * (W + 1)
+        for j in range(N):
+            for i in range(wt[j], W + 1):
+                dp[i] = max(dp[i], dp[i - wt[j]] + val[j])
+        return dp[W]
+
+# Nice Question.
+# Time Complexity: O(N*W)
+# Space Complexity: O(W)
