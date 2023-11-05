035fd1f7394f9f597874c69c15ac9963dd82c0f7
Asmit Singh <asmittbhardwaj@gmail.com>
2023-11-05 16:47:12 +0530

    Updated Top K Frequent Elements in Array.py

--- Top K Frequent Elements in Array.py
+++ Top K Frequent Elements in Array.py
@@ -8,7 +8,7 @@ Solved Using   : Python3
 from collections import Counter
 
 class Solution:
-    def topKFrequent(self, nums, k):
+    def topK(self, nums, k):
         count = Counter(nums)
         items = sorted(count.items(), key=lambda x: (-x[1], -x[0]))
         return [item[0] for item in items[:k]]
