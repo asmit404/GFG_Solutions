d79660732e35a65ac84196f88889443d3de0531b
Asmit Singh <asmittbhardwaj@gmail.com>
2023-11-26 18:59:23 +0530

    Create Print Pattern.py

---
+++ Print Pattern.py
@@ -0,0 +1,16 @@
+"""
+Title     : Print Pattern
+Author    : Asmit Singh
+Solved On   : 26 Nov 2023
+Solved Using   : Python3
+"""
+
+class Solution:
+    def pattern(self, N):
+        if N <= 0:
+            return [N,]
+        a = list(range(N, -5, -5))
+        return a + a[::-1][1:]
+
+# Time Complexity  : O(n)
+# Space Complexity : O(n)
