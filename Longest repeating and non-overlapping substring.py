a7efe0ee9bdb65266262071a3d7bb5613d499645
Asmit Singh <asmittbhardwaj@gmail.com>
2024-03-07 21:34:45 +0530

    Create Longest repeating and non-overlapping substring.py

---
+++ Longest repeating and non-overlapping substring.py
@@ -0,0 +1,24 @@
+'''
+Title     : Longest repeating and non-overlapping substring
+Author    : Asmit Singh
+Solved On   : 07 Mar 2024
+Solved Using   : Python3
+'''
+
+class Solution:
+    def longestSubstring(self, s, n):
+        res, maxi = "", -1
+        substr = ""
+        for i in range(n):
+            for j in range(i, n):
+                substr += s[j]
+                if substr in s[j+1:]:
+                    if maxi < len(substr):
+                        maxi = len(substr)
+                        res = substr
+                else:
+                    break
+        return res if res != "" else "-1"
+
+# Time Complexity: O(n ^ 3)
+# Space Complexity: O(n)
